4342e72873a9bf2abc99e8545e7b007dfde311c3
Bosco <bosco@MacBook-Air.hsd1.tn.comcast.net>
2019-06-05 01:01:23 -0500

    Updates

---
+++ precache-manifest.08f488b3afd290a27404a4b137c1fd11.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10b97e5314b1d6d436da3219edcabc99",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/clicky-game/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "effe6accb3b910a5f6f8",
+    "url": "/clicky-game/static/js/main.f86746a2.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
